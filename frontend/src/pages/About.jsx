00c09db4a426a12d2733f700ad2cb86cf0ec44de
Lekhansh Sachan <sachanlekhansh@gmail.com>
2024-11-11 01:32:47 +0530

    Added contact page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,47 +1,79 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div className='mx-10 tablet:mx-5 mobile:mx-2 '>
-        <h1 className='py-2 text-3xl font-normal text-center'>About Us</h1>
+    <div className="mx-10 tablet:mx-5 mobile:mx-2 ">
+      <h1 className="my-5 text-3xl font-normal text-center">About Us</h1>
 
-        {/*section top  */}
+      {/*section top  */}
 
-        <div className='flex  tablet: flex-col mobile:flex-col'>
-          <div className='w-1/4 tablet:flex tablet:justify-center tablet:items-center tablet:w-full mobile:w-full '>
-          <img className='w-full ' src={assets.about_image} alt="image" />
-          </div>
-          <div className='px-5 w-3/4  flex flex-col gap-4 justify-center tablet:w-full tablet:py-5 tablet:px-0 mobile:w-full mobile:px-2 mobile:py-5'>
-               <p className='text-sm text-slate-700'>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
-               <p className='text-sm text-slate-700'>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
-               <p className='font-bold'>Our Vision</p>
-               <p className='text-sm text-slate-700'>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
-          </div>
+      <div className="flex  tablet:flex-col mobile:flex-col">
+        {/* section-top-left */}
+
+        <div className="w-1/4 tablet:flex tablet:justify-center tablet:items-center tablet:w-full mobile:w-full ">
+          <img className="w-full rounded-lg" src={assets.about_image} alt="image" />
         </div>
 
-        {/*Section bottom */}
+        {/* section-top-right */}
 
-        <div>
-           <h2 className= 'my-5 text-lg font-medium text-slate-600'>Why <span className='text-black'>Choose Us</span></h2>
-           <div className='flex mobile:flex-col'>
-            <div className='p-10 border border-black tablet:p-5 mobile:p-5'>
-              <h2 className='text-[16px] font-medium my-2 '>Efficiency:</h2>
-              <p className='text-sm text-slate-700'>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
-            </div>
-            <div className='p-10 border border-black tablet:p-5 mobile:p-5 '>
-              <h2 className='text-[16px] font-medium my-2'>Convenience:</h2>
-              <p className='text-sm text-slate-700'>Access to a network of trusted healthcare professionals in your area.</p>
-            </div>
-            <div className='p-10  border border-black tablet:p-5  mobile:p-5 '>
-              <h2 className='text-[16px] font-medium my-2'> Personalization:</h2>
-              <p className='text-sm text-slate-700'>Tailored recommendations and reminders to help you stay on top of your health.</p>
-            </div>
-           </div>
+        <div className="px-10 w-3/4  flex flex-col gap-4 justify-center tablet:w-full tablet:py-5 tablet:px-0 mobile:w-full mobile:px-2 mobile:py-5">
+          <p className="text-sm text-slate-700">
+            Welcome to Prescripto, your trusted partner in managing your
+            healthcare needs conveniently and efficiently. At Prescripto, we
+            understand the challenges individuals face when it comes to
+            scheduling doctor appointments and managing their health records.
+          </p>
+          <p className="text-sm text-slate-700">
+            Prescripto is committed to excellence in healthcare technology. We
+            continuously strive to enhance our platform, integrating the latest
+            advancements to improve user experience and deliver superior
+            service. Whether you're booking your first appointment or managing
+            ongoing care, Prescripto is here to support you every step of the
+            way.
+          </p>
+          <p className="font-bold">Our Vision</p>
+          <p className="text-sm text-slate-700">
+            Our vision at Prescripto is to create a seamless healthcare
+            experience for every user. We aim to bridge the gap between patients
+            and healthcare providers, making it easier for you to access the
+            care you need, when you need it.
+          </p>
         </div>
+      </div>
 
+      {/*Section bottom */}
+
+      <div>
+        <h2 className="my-5 text-lg font-medium text-slate-600">
+          Why <span className="text-black">Choose Us</span>
+        </h2>
+        <div className="flex mobile:flex-col">
+          <div className="p-10 border border-black tablet:p-5 mobile:p-5">
+            <h2 className="text-[16px] font-medium my-2 ">Efficiency:</h2>
+            <p className="text-sm text-slate-700">
+              Streamlined appointment scheduling that fits into your busy
+              lifestyle.
+            </p>
+          </div>
+          <div className="p-10 border border-black tablet:p-5 mobile:p-5 ">
+            <h2 className="text-[16px] font-medium my-2">Convenience:</h2>
+            <p className="text-sm text-slate-700">
+              Access to a network of trusted healthcare professionals in your
+              area.
+            </p>
+          </div>
+          <div className="p-10  border border-black tablet:p-5  mobile:p-5 ">
+            <h2 className="text-[16px] font-medium my-2"> Personalization:</h2>
+            <p className="text-sm text-slate-700">
+              Tailored recommendations and reminders to help you stay on top of
+              your health.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <h1 className="my-5 text-3xl font-normal text-center">Contact Us</h1>
 
-export default Contact
+      <div className=" flex justify-center tablet:mx-5 mobile:mx-2">
+        <div className="w-[60%] tablet:w-full mobile:w-full">
+          <div className=" flex gap-10   mobile:mx-2 mobile:flex-col">
+
+            {/* section-left */}
+            <div className="w-1/2  mobile:w-full">
+              <img
+                className="h-full rounded-lg"
+                src={assets.contact_image}
+                alt="image"
+              />
+            </div>
+
+            {/* section - right */}
+
+            <div className="w-1/2 flex flex-col justify-center mobile:w-full ">
+              <div className="flex flex-col gap-4 mobile:items-center ">
+                <h2 className="font-semibold">OUR OFFICE</h2>
+                <p className="text-sm">
+                  54709 Willms Station <br /> Suite 350, Washington, USA
+                </p>
+                <div>
+                <div className="flex items-center gap-2">
+                  <i className="fa-solid fa-envelope text-blue-600"></i>
+                  <p className="text-sm">prescripto@gmail.com</p>
+                </div>
+                <div className="flex items-center gap-2 my-1">
+                  <i className="fa-solid fa-phone text-green-700"></i>
+                  <p className="text-sm">+91 950-663-1631</p>
+                </div>
+                </div>
+              </div>
+
+
+              <div className="mt-5 flex flex-col items-start gap-4  mobile:items-center mobile:text-center">
+                <h1 className="font-semibold">CAREERS AT PRESCRIPTO</h1>
+                <p>Learn more about our teams and job openings.</p>
+                <button className="text-sm border border-black p-2 hover:text-primary hover:border-primary">Explore Jobs</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
